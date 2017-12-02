9473e374e372f5b7c611dc1fe499e2ef3f640cea
James Hageman <jamesd@hageman.ca>
2017-12-02 12:07:51 -0500

    finish day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,35 +1,35 @@
 module Day1 where
 
-readDigit :: Char -> Integer
+readDigit :: Char -> Int
 readDigit c = read $ [c]
 
-digits :: String -> [Integer]
+digits :: String -> [Int]
 digits = fmap readDigit
 
 appendHead :: [a] -> [a]
 appendHead list = list ++ [head list]
 
-inAdjacentTuples :: [Integer] -> [(Integer, Integer)]
+inAdjacentTuples :: [Int] -> [(Int, Int)]
 inAdjacentTuples (x:y:rest) = (x,y) : inAdjacentTuples (y:rest)
 inAdjacentTuples _ = []
 
-inHalfwayTuples :: [Integer] -> [(Integer, Integer)]
+inHalfwayTuples :: [Int] -> [(Int, Int)]
 inHalfwayTuples list = fmap (\(x, i) ->
-    (x, (list !! (i + size `div` 2) `mod` size))
+    (x, (list !! ((i + size `div` 2) `mod` size)))
   ) listWithIndices
   where
     listWithIndices = zip list [0..]
     size = length list
 
-sumMatchingDigits :: [(Integer, Integer)] -> Integer
+sumMatchingDigits :: [(Int, Int)] -> Int
 sumMatchingDigits ((x,y):rest) =
   if x == y then
     x + sumMatchingDigits rest
     else sumMatchingDigits rest
 sumMatchingDigits [] = 0
 
-day1part1 :: String -> Integer
+day1part1 :: String -> Int
 day1part1 = sumMatchingDigits . inAdjacentTuples . appendHead . digits
 
-day1part2 :: String -> Integer
+day1part2 :: String -> Int
 day1part2 = sumMatchingDigits . inHalfwayTuples . digits
