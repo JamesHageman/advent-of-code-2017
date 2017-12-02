47d6ac10e67e9bf5f5217c6380c87000cd4cb457
James Hageman <jamesd@hageman.ca>
2017-12-02 12:51:40 -0500

    Day 2 part 2

--- src/Day2.hs
+++ src/Day2.hs
@@ -16,3 +16,30 @@ checksum = sum . fmap rowRange
 
 day2part1 :: FilePath -> IO Int
 day2part1 = fmap (checksum . parseSpreadsheet) . readFile
+
+a `divides` b = b `mod` a == 0
+
+firstDivisor :: [Int] -> Int -> Maybe Int
+firstDivisor (x:xs) y =
+  if x `divides` y then
+    Just x
+    else firstDivisor xs y
+firstDivisor [] y = Nothing
+
+findDivisiblePair :: [Int] -> Maybe (Int, Int)
+findDivisiblePair fullList = search fullList fullList
+  where
+    search fullList (x:xs) =
+      case firstDivisor (filter (/= x) fullList) x of
+        Just y -> Just (x, y)
+        Nothing -> search fullList xs
+    search fullList [] = Nothing
+
+rowQuotient :: [Int] -> Int
+rowQuotient row =
+  case findDivisiblePair row of
+    Just (hi, lo) -> hi `div` lo
+    Nothing -> 0
+
+day2part2 :: FilePath -> IO Int
+day2part2 = fmap (sum . fmap rowQuotient . parseSpreadsheet) . readFile
