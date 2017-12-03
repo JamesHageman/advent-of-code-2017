712bda162c56bc84624587aa71cdfb6e7b7116de
James Hageman <jamesd@hageman.ca>
2017-12-03 13:25:11 -0500

    day3 part 2

--- src/Day3.hs
+++ src/Day3.hs
@@ -1,5 +1,9 @@
 module Day3 where
 
+import qualified Data.Map.Strict as Map
+import Control.Monad (foldM)
+import Data.Monoid
+
 spiral :: [(Int, Int)]
 spiral = spiralFrom (0, 0)
 
@@ -30,3 +34,23 @@ test =
     (3,-2) == positionOf 26,
     (3,-1) == positionOf 27
   ]
+
+add :: (Int, Int) -> (Int, Int) -> (Int, Int)
+add (a, b) (c, d) = (a + c, b + d)
+
+neighbors :: (Int, Int) -> [(Int, Int)]
+neighbors pos =
+  fmap (add pos) $ take 8 $ spiralFrom (1, 0)
+
+sumMaybe :: [Maybe Int] -> Int
+sumMaybe xs = getSum $ foldMap (maybe mempty Sum) xs
+
+spiralWithNeighborSum :: Map.Map (Int, Int) Int -> (Int, Int) -> [(Int, (Int, Int))]
+spiralWithNeighborSum state curr =
+  (sum, curr) : spiralWithNeighborSum nextState (nextPosition curr)
+  where
+    sum = if curr == (0, 0) then 1 else
+      sumMaybe $ fmap (\pos -> Map.lookup pos state) $ neighbors curr
+    nextState = Map.insert curr sum state
+
+day3part2 = fst $ head $ filter ((> 325489) . fst) $ spiralWithNeighborSum Map.empty (0, 0)
