d0d3ab46d9e9264ff70873e13255803e417fbc3e
James Hageman <james.hageman@shopify.com>
2017-12-04 16:47:29 -0500

    day 4

---
+++ src/Day4.hs
@@ -0,0 +1,33 @@
+module Day4 where
+
+import Data.Set as Set
+import Data.Monoid
+import Data.List
+
+data Passphrase = Passphrase [String] (Set.Set String) deriving (Show)
+
+validate1 :: Passphrase -> Bool
+validate1 (Passphrase list set) = Set.size set == length list
+
+validate2 :: Passphrase -> Bool
+validate2 (Passphrase list set) = Set.size set2 == length list
+  where set2 = Set.foldr (Set.insert . sort) Set.empty set
+
+parsePassphrase :: (Passphrase -> Bool) -> String -> Maybe Passphrase
+parsePassphrase validator line = 
+  if validator passphrase then
+    Just passphrase
+    else Nothing
+  where
+    passwords = words line
+    setOfPasswords = Set.fromList passwords
+    passphrase = Passphrase passwords setOfPasswords
+
+parsePassphrases :: (Passphrase -> Bool) -> String -> [Maybe Passphrase]
+parsePassphrases validator = fmap (parsePassphrase validator) . lines
+
+countValid :: [Maybe a] -> Int
+countValid xs = getSum $ foldMap (maybe mempty (const (Sum 1))) xs
+
+day4part1 = countValid . parsePassphrases validate1 <$> readFile "test/Day4.input"
+day4part2 = countValid . parsePassphrases validate2 <$> readFile "test/Day4.input"
--- src/Lib.hs
+++ src/Lib.hs
@@ -5,6 +5,7 @@ module Lib
 import Day1
 import Day2
 import Day3
+import Day4
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
